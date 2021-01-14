69c48d7b6379780fc01911d6fdd6a4748bc337f3
chrisstina <chrisstina@gmail.com>
2021-01-14 12:31:35 +0200

    test fixes

--- tests/statsModuleUnitTest.js
+++ tests/statsModuleUnitTest.js
@@ -22,55 +22,55 @@ describe('Stats module', () => {
     });
 
     it('should list api stats for profile', async function () {
-        const statsRequestsTodayAll = await statsModule.getAPIStats('request');
+        const statsRequestsTodayAll = await statsModule.getAPITotalHits('request');
         expect(statsRequestsTodayAll).to.have.property('hash', `apirequests:allprofiles:${moment().format('YYYY')}:${moment().format('MM')}:${moment().format('DD')}`);
         expect(statsRequestsTodayAll).to.have.property('flights').which.is.finite;
         expect(statsRequestsTodayAll).to.have.property('price').which.is.finite;
 
-        const statsErrorsTodayAll = await statsModule.getAPIStats('error');
+        const statsErrorsTodayAll = await statsModule.getAPITotalHits('error');
         expect(statsErrorsTodayAll).to.have.property('hash', `apierrors:allprofiles:${moment().format('YYYY')}:${moment().format('MM')}:${moment().format('DD')}`);
         expect(statsErrorsTodayAll).to.have.property('flights').which.is.finite;
         expect(statsErrorsTodayAll).to.have.property('price').which.is.finite;
 
-        const statsRequestsThisMonthDefault = await statsModule.getAPIStats('request', 'default', 'month');
+        const statsRequestsThisMonthDefault = await statsModule.getAPITotalHits('request', 'default', 'month');
         expect(statsRequestsThisMonthDefault).to.have.property('hash', `apirequests:default:${moment().format('YYYY')}:${moment().format('MM')}`);
         expect(statsRequestsThisMonthDefault).to.have.property('flights').which.is.finite;
         expect(statsRequestsThisMonthDefault).to.have.property('price').which.is.finite;
 
-        const statsErrorsSomeWeekDefault = await statsModule.getAPIStats('error', 'default', 'week', '2020W35');
+        const statsErrorsSomeWeekDefault = await statsModule.getAPITotalHits('error', 'default', 'week', '2020W35');
         expect(statsErrorsSomeWeekDefault).to.have.property('hash', `apierrors:default:2020:W35`);
         expect(statsErrorsSomeWeekDefault).to.have.property('flights').which.is.finite;
         expect(statsErrorsSomeWeekDefault).to.have.property('price').which.is.finite;
 
-        const statsRequestsLastYearAll = await statsModule.getAPIStats('request', null, 'year');
+        const statsRequestsLastYearAll = await statsModule.getAPITotalHits('request', null, 'year');
         expect(statsRequestsLastYearAll).to.have.property('hash', `apirequests:allprofiles:2021`);
         expect(statsRequestsLastYearAll).to.have.property('flights').which.is.finite;
         expect(statsRequestsLastYearAll).to.have.property('price').which.is.finite;
     });
 
     it('should list api stats across all profiles', async function () {
-        const stats = await statsModule.getAPICallsStatsByProfile('day', '2020-10-23');
+        const stats = await statsModule.getAPITotalHitsByProfile('day', '2020-10-23');
         expect(stats).to.have.property('allprofiles');
         expect(stats).to.have.property('default');
         expect(stats).to.have.property('ttservice');
         expect(stats).to.have.property('farf');
         expect(stats.allprofiles).to.have.property('hash', 'apirequests:allprofiles:2020:10:23');
 
-        const statsToday = await statsModule.getAPICallsStatsByProfile('day');
+        const statsToday = await statsModule.getAPITotalHitsByProfile('day');
         expect(statsToday).to.have.property('allprofiles');
         expect(statsToday).to.have.property('default');
         expect(statsToday).to.have.property('ttservice');
         expect(statsToday).to.have.property('farf');
         expect(statsToday.allprofiles).to.have.property('hash', `apirequests:allprofiles:${moment().format('YYYY')}:${moment().format('MM')}:${moment().format('DD')}`);
 
-        const statsMonth = await statsModule.getAPICallsStatsByProfile('month');
+        const statsMonth = await statsModule.getAPITotalHitsByProfile('month');
         expect(statsMonth).to.have.property('allprofiles');
         expect(statsMonth).to.have.property('default');
         expect(statsMonth).to.have.property('ttservice');
         expect(statsMonth).to.have.property('farf');
         expect(statsMonth.allprofiles).to.have.property('hash', `apirequests:allprofiles:${moment().format('YYYY')}:${moment().format('MM')}`);
 
-        const statsMonthNov19 = await statsModule.getAPICallsStatsByProfile('month', '201911');
+        const statsMonthNov19 = await statsModule.getAPITotalHitsByProfile('month', '201911');
         const november19 = moment('2019-11-01');
         expect(statsMonthNov19).to.have.property('allprofiles');
         expect(statsMonthNov19).to.have.property('default');
@@ -101,7 +101,7 @@ describe('Stats module', () => {
         }
     });
 
-    it('should fail to return avg response time for fake precision', async function () {
-        expect(statsModule.getAPIResponseTime('flights', '1')).to.eventually.throw();
+    it('should fail to return avg response time for fake precision', function (done) {
+        expect(statsModule.getAPIResponseTime('flights', '1')).to.eventually.be.rejected.and.notify(done);
     });
 });
